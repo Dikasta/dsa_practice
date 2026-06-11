71bfd3a6bfa4bff8bc1b87a05b162974285f6886
Deepak Maurya <75212895+Dikasta@users.noreply.github.com>
2026-06-11 10:51:10 +0530

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -1,1 +1,4 @@
+'''Option A: Breadth-First Search (BFS) — Best for Unweighted Mazes
+If every step the mouse takes costs the exact same amount of energy, BFS is the perfect choice.
 
+How it works: The mouse explores the maze layer by layer, expanding equally in all directions (up, down, left, right) from its starting point.'''
